c8177bca9910b5b51fdae9d487f6a7baf2549295
Carlos Hurtado <carlosstevenh@hotmail.com>
2026-05-01 12:32:42 -0500

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://applications-web-personal.github.io',
+  base: '/menu-solo-perros',
   vite: {
     plugins: [tailwindcss()],
   },
